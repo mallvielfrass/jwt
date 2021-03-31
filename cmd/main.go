360f925ac1361ac34295f3234c1ed2dcaa7afbf9
mallvielfrass <masterharoldsaxon4@gmail.com>
2021-03-31 12:38:32 +0300

    add routers and security access

--- cmd/main.go
+++ cmd/main.go
@@ -25,9 +25,21 @@ func main() {
 	}
 	//handlers
 	r.HandleFunc("/", st.index)
-	r.HandleFunc("/create_user", st.add)
-	r.HandleFunc("/t", st.t)
-	r.HandleFunc("/s", st.s)
+	r.HandleFunc("/login", st.login)
+	r.Route("/api", func(r chi.Router) {
+		//only to this method must be acces for not authorized user
+		r.With(middleware.NoAuthMiddleware).Route("/", func(r chi.Router) {
+			r.HandleFunc("/create_user", st.CreateUserApi)
+			r.HandleFunc("/create_session", st.CreateSessionApi)
+			r.HandleFunc("/search_session", st.SearchSessionApi) //for tesing
+		})
+
+		//to this methods must be acces for only authorized user
+		r.With(middleware.AuthMiddleware).Route("/", func(r chi.Router) {
+			r.HandleFunc("/info", st.Info)
+		})
+	})
+	//logger
 	n := negroni.New()
 	n.Use(negroni.HandlerFunc(middleware.Middleware))
 	n.UseHandler(r)
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -7,8 +7,24 @@ import (
 )
 
 func Middleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	// do some stuff before
 	fmc.Printfln("#bbtmiddleware> #ybturl #bbt[#gbt%s#bbt] #ybtmethod #bbt[#gbt%s#bbt]", r.URL.Path, r.Method)
 	next(rw, r)
-	// do some stuff after
+}
+func AuthMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmc.Printfln("#bbtAuthMiddleware> #ybturl #bbt[#gbt%s#bbt] #ybtmethod #bbt[#gbt%s#bbt]", r.URL.Path, r.Method)
+
+		next.ServeHTTP(w, r)
+	})
+
+}
+func NoAuthMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmc.Printfln("#bbtNoAuthMiddleware> #ybturl #bbt[#gbt%s#bbt] #ybtmethod #bbt[#gbt%s#bbt]", r.URL.Path, r.Method)
+
+		next.ServeHTTP(w, r)
+	})
+
 }
